f8eead12384d5163a71d0f08136b17a52d4b6e83
box16 <inoinobox16@gmail.com>
2025-09-24 23:00:13 +0900

    Like画面のカード表示改善

--- front/src/components/LikedArticleCard.tsx
+++ front/src/components/LikedArticleCard.tsx
@@ -9,15 +9,17 @@ export function LikedArticleCard({ item }: LikedArticleCardProps) {
   return (
     <article className="liked-card">
       {item.source ? <div className="liked-card__source">{item.source}</div> : null}
-      <a
-        href={item.link}
-        target="_blank"
-        rel="noreferrer"
-        className="liked-card__title"
-      >
-        {item.title}
-      </a>
-      <SanitizedHtml html={item.summary} className="liked-card__summary" />
+      <div className="liked-card__body">
+        <a
+          href={item.link}
+          target="_blank"
+          rel="noreferrer"
+          className="liked-card__title"
+        >
+          {item.title}
+        </a>
+        <SanitizedHtml html={item.summary} className="liked-card__summary" />
+      </div>
     </article>
   );
 }
